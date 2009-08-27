5908a43fe18c8efdb35996e929ce4489091c7064
paul <paul@intridea.com>
2009-08-26 23:11:34 -0400

    read works with CSV and FasterCSV

--- final.rb
+++ final.rb
@@ -6,7 +6,7 @@ username, password = ARGV[0], ARGV[1]
 raise ArgumentError, "No username provided" unless username
 raise ArgumentError, "No password provided" unless password
 
-lines = FasterCSV.parse(File.open("final_csv.csv", "r")).flatten
+lines = FasterCSV.parse(File.read("final_csv.csv", "r")).flatten
 letters = lines.map{|line| line.split('').last}
 puts message = letters.join.gsub('_', ' ')
 
