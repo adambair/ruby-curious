893935e0ab6c9d750f18f4cef6a4b24859beeca4
Adam Bair <adambair@gmail.com>
2009-10-26 15:53:41 -0400

    Update lab 04 description

--- labs/lab_04_full_on_pig_latin.rb
+++ labs/lab_04_full_on_pig_latin.rb
@@ -1,8 +1,9 @@
 # --------------------------
 # Lab 04: Full on Pig Latin
 # --------------------------
-# prompt the user for a sentence
-# print out the sentence translated to pig latin
+# Using some of the code created in Lab 03
+# Prompt the user for a sentence
+# Print out the sentence translated to pig latin
 # --------------------------
 # Pig Latin algorithm
 # take the first letter of a word
